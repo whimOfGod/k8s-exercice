98181cac62c2fbdb106e96d962aba262ef87257b
whimofgod <aaronkolins98@gmail.com>
2024-05-23 22:25:50 +0200

    [ Exercice ] [ 5 ] : On mélange tout,  configuration qui exécute une centaine de tâches dispatchées

--- launch_global.sh
+++ launch_global.sh
@@ -9,6 +9,18 @@ cd ../worker/
 docker build -t aaronkolins77/worker .
 docker push aaronkolins77/worker
 
+kubectl apply -f worker-deployment.yaml
 kubectl apply -f worker-add-deployment.yaml
 kubectl apply -f worker-mult-deployment.yaml
 kubectl apply -f worker-service.yaml
+
+echo "Waiting for services to start..."
+sleep 30
+
+kubectl get pods
+
+kubectl get services
+
+planner_pod=$(kubectl get pods --selector=app=planner --output=jsonpath='{.items[0].metadata.name}')
+echo "Logs of planner pod: $planner_pod"
+kubectl logs $planner_pod
--- planner/main.js
+++ planner/main.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 const express = require('express')
 
 const port = 1337
-const nbTasks = parseInt(process.env.TASKS) || 50 
+const nbTasks = parseInt(process.env.TASKS) || 100 
 
 const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
 const taskType = () => (randInt(0, 2) ? 'mult' : 'add')
